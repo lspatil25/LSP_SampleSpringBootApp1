3fb99028cde509199742f03835473dbf2f028426
Lalit Patil <Lalit.Patil@aurionpro.com>
2017-10-16 16:22:28 +0530

    First commit

---
+++ src/main/java/com/lsp/LSP_SampleSpringBootApp1/LspSampleSpringBootApp1Application.java
@@ -0,0 +1,12 @@
+package com.lsp.LSP_SampleSpringBootApp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LspSampleSpringBootApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LspSampleSpringBootApp1Application.class, args);
+	}
+}
---
+++ src/test/java/com/lsp/LSP_SampleSpringBootApp1/LspSampleSpringBootApp1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lsp.LSP_SampleSpringBootApp1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LspSampleSpringBootApp1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
